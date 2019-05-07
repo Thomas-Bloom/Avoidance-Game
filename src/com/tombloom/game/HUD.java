3b0ea86eebe5fb4d6eabd5f9f1fc99c0897b059d
Tom Bloom <thomasbloom25@gmail.com>
2019-05-07 14:45:39 +0100

    Health bar changes colour when damage taken

--- src/com/tombloom/game/HUD.java
+++ src/com/tombloom/game/HUD.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class HUD {
     private final int healthPosX = 15, healthPosY = 15;
     private final int healthWidth = 200, healthHeight = 30;
-
+    
     public void tick(){
     }
 
@@ -14,7 +14,7 @@ public class HUD {
         g.setColor(Color.gray);
         g.fillRect(healthPosX, healthPosY, healthWidth, healthHeight);
 
-        g.setColor(Color.green);
+        g.setColor(Color.getHSBColor((1f * GameState.health) / 360, 1f, 1f));
         g.fillRect(healthPosX, healthPosY, GameState.health * 2 , healthHeight);
 
         g.setColor(Color.white);
